83e400d271271fcefdbe15b3754784fcb3a2cca8
ugo510 <ummagohil@gmail.com>
2019-02-22 11:57:32 +0000

    redux working

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,33 @@ import StockPrices from './StockPrices'
 import FX from './FX'
 import SectorPerf from './SectorPerf'
 import Crypto from './Crypto'
+import { simpleAction } from '../actions/simpleAction';
+import { connect } from 'react-redux';
 import './App.css';
 
+const mapStateToProps = state => ({
+  ...state
+ })
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
 class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+   }
   render() {
     return (
       <div className="App">
         app
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <pre>
+ {
+  JSON.stringify(this.props)
+ }
+</pre>
+
         <StockPrices />
         <FX />
         <SectorPerf />
@@ -19,4 +39,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
